c9cbdd7cf998955aa7ed9f5a715a544aa850b745
Toomas Pappel <toomas.pappel@smit.ee>
2017-11-14 10:35:02 +0200

    kuues_redis

--- 12f_app/src/main/java/com/github/tpappel/app/StockSpringBootStarter.java
+++ 12f_app/src/main/java/com/github/tpappel/app/StockSpringBootStarter.java
@@ -2,7 +2,9 @@ package com.github.tpappel.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession 
 @SpringBootApplication
 public class StockSpringBootStarter {
     public static void main(String[] args) {
